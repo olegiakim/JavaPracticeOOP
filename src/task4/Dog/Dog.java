9556073e0323c112d1e76d1ae4fa4314b2f03ec4
Oleg Iakim <execute067@gmail.com>
2021-08-14 16:14:35 -0700

    Updated Task4

--- src/task4/Dog/Dog.java
+++ src/task4/Dog/Dog.java
@@ -1,4 +1,25 @@
 package task4.Dog;
 
 public class Dog {
+
+    String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public void twoPlusTwo() {
+        int a = 2;
+        int b = 2;
+        System.out.println( a + b);
+    }
+
+    public void sumTwoNumber(int number1, int number2) {
+        System.out.println(number1 + number2);
+    }
+
+    public int sum(int a, int b) {
+        int c = a + b;
+        return c;
+    }
 }
--- src/task4/Dog/Main.java
+++ src/task4/Dog/Main.java
@@ -1,4 +1,17 @@
 package task4.Dog;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Dog dog1 = new Dog("Jack");
+        //
+        dog1.twoPlusTwo();
+        //
+        dog1.sumTwoNumber(10, 20);
+        //
+        int result = dog1.sum(5, 5);
+        System.out.println(result);
+        System.out.println(result * 2);
+
+    }
 }
