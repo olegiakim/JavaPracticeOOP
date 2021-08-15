c1ddadf0baeaf3a46274842146830398ad5f4283
Oleg Iakim <execute067@gmail.com>
2021-08-14 17:04:43 -0700

    Updated work on Task6

--- src/task6/Office_Encapsulation/Candidate.java
+++ src/task6/Office_Encapsulation/Candidate.java
@@ -1,4 +1,22 @@
 package task6.Office_Encapsulation;
 
 public class Candidate {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private boolean is_degree;
+
+
+    public Candidate(String firstName, String lastName, int age, boolean is_degree) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.is_degree = is_degree;
+    }
+
+
+
+
+
 }
--- src/task6/Office_Encapsulation/Main.java
+++ src/task6/Office_Encapsulation/Main.java
@@ -1,4 +1,17 @@
 package task6.Office_Encapsulation;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Candidate candidate1 = new Candidate("John", "Smith", 35, true);
+
+
+//        System.out.println(candidate1.lastName);
+//
+//        candidate1.lastName = "Black";
+//
+//        System.out.println(candidate1.lastName);
+
+    }
 }
