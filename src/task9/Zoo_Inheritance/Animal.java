23617fb36fff548fc5bed0a9fb0449c887a9d70e
Oleg Iakim <execute067@gmail.com>
2021-08-16 11:01:05 -0700

    Updated work on Task9 Inheritance

--- src/task9/Zoo_Inheritance/Animal.java
+++ src/task9/Zoo_Inheritance/Animal.java
@@ -1,6 +1,6 @@
 package task9.Zoo_Inheritance;
 
-public class Animal {
+public abstract class Animal {
 
     public String name;
     public int age;
---
+++ src/task9/Zoo_Inheritance/Main.java
@@ -0,0 +1,23 @@
+package task9.Zoo_Inheritance;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Cat cat1 = new Cat("Lucky", 5, true);
+
+        Dog dog1 = new Dog("Rex", 4, "german shepherd");
+
+        Rabbit rabbit1 = new Rabbit("Snow Ball", 2, true);
+
+
+
+//      It is abstract
+//      Animal animal1 = new Animal("Dolphin", 10);
+
+
+
+
+    }
+}
